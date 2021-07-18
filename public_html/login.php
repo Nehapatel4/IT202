9d3fa11add694948c20d31267cee46357565064d
Nehapatel4 <84941247+Nehapatel4@users.noreply.github.com>
2021-07-18 11:39:39 -0400

    Update login.php

--- public_html/login.php
+++ public_html/login.php
@@ -3,3 +3,14 @@
 echo "There was a validation issue";
 
 ?>
+<ul>
+    <li><a href="home.php">Home</a></li>
+    <?php if (!is_logged_in()): ?>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+    <?php endif; ?>
+    <?php if (is_logged_in()): ?>
+        <li><a href="profile.php">Profile</a></li>
+        <li><a href="logout.php">Logout</a></li>
+    <?php endif; ?>
+</ul>
